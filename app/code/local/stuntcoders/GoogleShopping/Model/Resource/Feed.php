e8f757c6209b95da94b41467b1e8a92634980ca8
Ozren Srdić <ozren@stuntcoders.com>
2016-03-11 18:15:28 +0100

    Remove invalid named folder

--- app/code/local/stuntcoders/GoogleShopping/Model/Resource/Feed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Stuntcoders_GoogleShopping_Model_Resource_Feed extends Mage_Core_Model_Resource_Db_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('stuntcoders_googleshopping/feed', 'id');
-    }
-}
