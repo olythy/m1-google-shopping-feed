c7d2221505eeaeeec2bc9a596848b957caa4fe3f
arioch1984 <arioch1984@gmail.com>
2016-03-17 18:18:18 +0100

    added subvlaues

--- app/code/local/Stuntcoders/GoogleShopping/Model/Feed.php
+++ app/code/local/Stuntcoders/GoogleShopping/Model/Feed.php
@@ -2,95 +2,135 @@
 
 class Stuntcoders_GoogleShopping_Model_Feed extends Mage_Core_Model_Abstract
 {
-    protected function _construct()
-    {
-        $this->_init('stuntcoders_googleshopping/feed');
-    }
+	protected function _construct()
+	{
+		$this->_init('stuntcoders_googleshopping/feed');
+	}
 
-    public function validate()
-    {
-        $errors = array();
-        if (!$this->getPath()) {
-            $errors[] = Mage::helper('stuntcoders_googleshopping')->__('Path is mandatory');
-        }
+	public function validate()
+	{
+		$errors = array();
+		if (!$this->getPath()) {
+			$errors[] = Mage::helper('stuntcoders_googleshopping')->__('Path is mandatory');
+		}
 
-        return $errors;
-    }
+		return $errors;
+	}
 
-    public function generateXml()
-    {
-        $productCollection = Mage::getModel('catalog/product')
-            ->getCollection()
-            ->joinField(
-                'category_id', 'catalog/category_product', 'category_id',
-                'product_id = entity_id', null, 'left'
-            )
-            ->addAttributeToSelect('*')
-            ->addAttributeToFilter('category_id', array('in' => explode(',', $this->getCategories())))
-            ->groupByAttribute('entity_id');
+	public function generateXml()
+	{
+		$productCollection = Mage::getModel('catalog/product')
+		                         ->getCollection()
+		                         ->joinField(
+			                         'category_id', 'catalog/category_product', 'category_id',
+			                         'product_id = entity_id', null, 'left'
+		                         )
+		                         ->addAttributeToSelect('*')
+		                         ->addAttributeToFilter('category_id', array('in' => explode(',', $this->getCategories())))
+		                         ->groupByAttribute('entity_id');
 
-        $doc = new DOMDocument('1.0');
-        $doc->formatOutput = true;
+		$doc = new DOMDocument('1.0');
+		$doc->formatOutput = true;
 
-        $rss = $doc->appendChild($doc->createElement('rss'));
-        $rss->setAttribute('xmlns:g', '');
-        $rss->setAttribute('version', '2.0');
-        $channel = $rss->appendChild($doc->createElement('channel'));
-        $channel->appendChild($doc->createElement('title', $this->getTitle()));
-        $channel->appendChild($doc->createElement('link', Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB)));
-        $channel->appendChild($doc->createElement('description', $this->getDescription()));
+		$rss = $doc->appendChild($doc->createElement('rss'));
+		$rss->setAttribute('xmlns:g', 'http://base.google.com/ns/1.0');
+		$rss->setAttribute('version', '2.0');
+		$channel = $rss->appendChild($doc->createElement('channel'));
+		$channel->appendChild($doc->createElement('title', $this->getTitle()));
+		$channel->appendChild($doc->createElement('link', Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB)));
+		$channel->appendChild($doc->createElement('description', $this->getDescription()));
 
-        $attributes = json_decode($this->getAttributes(), true);
-        foreach ($productCollection as $product) {
-            $item = $channel->appendChild($doc->createElement('item'));
+		$attributes = json_decode($this->getAttributes(), true);
+		foreach ($productCollection as $product) {
+			$item = $channel->appendChild($doc->createElement('item'));
 
-            foreach ($attributes as $name => $value) {
-                $tagValue = isset($value['default']) ? $value['default'] : '';
-                $prefix = '';
-                $valuePrefix = '';
+			foreach ($attributes as $name => $value) {
+				$tagValue = isset($value['default']) ? $value['default'] : '';
+				$prefix = '';
+				$valuePrefix = '';
 
-                if (!empty($value['attribute']) && $product->getData($value['attribute'])) {
-                    //$tagValue = $product->getAttributeText($value['attribute']);
-                    $tagValue = $product->getData($value['attribute']);
-                }
+				$itemTag = false;
+				foreach($value as $subName => $subValue) {
+					if(is_array($subValue)){
+						$subTagValue = isset($subValue['default']) ? $subValue['default'] : '';
 
-                if (empty($tagValue)) {
-                    continue;
-                }
+						if (!empty($value['prefix'])) {
+							$prefix = $value['prefix'] . ':';
+						}
 
-                if (!empty($value['prefix'])) {
-                    $prefix = $value['prefix'] . ':';
-                }
+						if(!$itemTag) {
+							$itemTag = $item->appendChild($doc->createElement($prefix . $name, ''));
 
-                if (!empty($value['value_prefix'])) {
-                    $valuePrefix = $value['value_prefix'];
-                }
+							if (!empty($value['type'])) {
+								$itemTag->setAttribute('type', $value['type']);
+							}
+						}
 
-                $itemTag = $item->appendChild($doc->createElement($prefix . $name, $valuePrefix . $tagValue));
+						if (!empty($value['attribute']) && $product->getData($value['attribute'])) {
+							$subTagValue = $product->getData($subValue['attribute']);
+						}
 
-                if (!empty($value['type'])) {
-                    $itemTag->setAttribute('type', $value['type']);
-                }
-            }
+						if (empty($subTagValue)) {
+							continue;
+						}
 
-	        if($product->isInStock()) {
-		        $item->appendChild($doc->createElement('g:availability', 'in stock'));
-	        }
-	        else {
-		        $item->appendChild($doc->createElement('g:availability', 'out of stock'));
-	        }
+						if (!empty($subValue['prefix'])) {
+							$prefix = $subValue['prefix'] . ':';
+						}
 
-            $item->appendChild($doc->createElement('g:link', $product->getUrlInStore()));
-            $item->appendChild($doc->createElement(
-                'g:image_link',
-                Mage::helper('catalog/image')->init($product, 'image')->resize(800)
-            ));
-            $item->appendChild($doc->createElement(
-                'g:price',
-                Mage::helper('core')->currency($product->getPrice(), true, false)
-            ));
-        }
+						if (!empty($subValue['value_prefix'])) {
+							$valuePrefix = $subValue['value_prefix'];
+						}
 
-        return $doc->saveXML();
-    }
+						$subItemTag = $itemTag->appendChild($doc->createElement($prefix . $subName, $valuePrefix . $subTagValue));
+
+						if (!empty($value['type'])) {
+							$subItemTag->setAttribute('type', $subValue['type']);
+						}
+					}
+				}
+
+				if (!empty($value['attribute']) && $product->getData($value['attribute'])) {
+					$tagValue = $product->getData($value['attribute']);
+				}
+
+				if (empty($tagValue)) {
+					continue;
+				}
+
+				if (!empty($value['prefix'])) {
+					$prefix = $value['prefix'] . ':';
+				}
+
+				if (!empty($value['value_prefix'])) {
+					$valuePrefix = $value['value_prefix'];
+				}
+
+				$itemTag = $item->appendChild($doc->createElement($prefix . $name, $valuePrefix . $tagValue));
+
+				if (!empty($value['type'])) {
+					$itemTag->setAttribute('type', $value['type']);
+				}
+			}
+
+			if($product->isInStock()) {
+				$item->appendChild($doc->createElement('g:availability', 'in stock'));
+			}
+			else {
+				$item->appendChild($doc->createElement('g:availability', 'out of stock'));
+			}
+
+			$item->appendChild($doc->createElement('g:link', $product->getUrlInStore()));
+			$item->appendChild($doc->createElement(
+				'g:image_link',
+				Mage::helper('catalog/image')->init($product, 'image')->resize(800)
+			));
+			$item->appendChild($doc->createElement(
+				'g:price',
+				Mage::helper('core')->currency($product->getPrice(), true, false)
+			));
+		}
+
+		return $doc->saveXML();
+	}
 }
