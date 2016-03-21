2399db8fe0a3f43f64473cba829aec1570132db9
arioch1984 <arioch1984@gmail.com>
2016-03-21 09:43:10 +0100

    debug: added category and google category to the feed

--- app/code/local/Stuntcoders/GoogleShopping/Model/Feed.php
+++ app/code/local/Stuntcoders/GoogleShopping/Model/Feed.php
@@ -167,10 +167,10 @@ class Stuntcoders_GoogleShopping_Model_Feed extends Mage_Core_Model_Abstract
 		foreach ($_product->getCategoryCollection() as $category) {
 
 			$cat = Mage::getModel('catalog/category')->load($category->getId());
-			$response[] = $cat->getGoogleCategory();
+			$response = $cat->getGoogleCategory();
 
 		}
-		$response = implode(" > ",$response);
+		//$response = implode(" > ",$response);
 		return $response;
 
 	}
